8a6f55314534905d2a27b7e550f5e6dd3ca9045a
Vikalp Malhotra <kimboho@gmail.com>
2021-12-14 13:08:03 -0500

    commit tree c3d8c963a771de520373ee4ed7bf5cead92e8d9a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'Soni'
+
+
+
+upperCaseCustomerName = () => {
+    customerName = customerName.toUpperCase();
+}
+
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'maybe bob';
+}
